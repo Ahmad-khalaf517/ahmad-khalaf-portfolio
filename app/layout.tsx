d6159746e9b7b4797d75768ac830715c56adceae
Ahmad Khalaf <ahmadkhalaf517@gmail.com>
2026-08-15 05:00:06 +0300

    Remove update-banner component not needed

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import { UpdateBanner } from "@/components/app/update-banner";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -31,7 +30,6 @@ export default function RootLayout({
     >
       <body className="min-h-full flex flex-col">
         {children}
-        <UpdateBanner />
       </body>
     </html>
   );
